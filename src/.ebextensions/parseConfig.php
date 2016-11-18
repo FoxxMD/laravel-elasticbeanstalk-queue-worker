dc48509f260db2443e8eaa1a22ec508898bcf1af
FoxxMD <matt.duncan13@gmail.com>
2016-11-18 13:38:35 -0500

    Convert env var keys to lower-case
    Now environmental variables don't have to be case-sensitive! Fixes #1

--- src/.ebextensions/parseConfig.php
+++ src/.ebextensions/parseConfig.php
@@ -23,22 +23,23 @@ if(file_exists('/var/app/ondeck/jsonEnv')){
 } else if(file_exists('/var/app/current/jsonEnv')){
     $envLocation = '/var/app/current/jsonEnv';
 }
-$envVars = json_decode(file_get_contents($envLocation), true);
+$vars = json_decode(file_get_contents($envLocation), true);
+$envVars = array_change_key_case($vars); // convert keys to lower case so environmental variables don't have to be case-sensitive
 
 $programs = '';
 
 foreach($envVars as $key => $val){
-    if(strpos(strtolower($key), 'queue') !== false && strpos(strtolower($key), 'queue_driver') === false){
-        $tryKey = substr($key, 10) . 'Tries'; //get queue $key + tries to see if custom tries is set
-        $sleepKey = substr($key, 5) . 'Sleep'; //get queue $key + sleep to see if custom sleep is set
-        $numProcKey = substr($key, 5) . 'NumProcs'; //get queue $key + num process to see if custom number of processes is set
-        $startSecsKey = substr($key, 5) . 'StartSecs'; //get queue $key + number of seconds the process should stay up
+    if(strpos($key, 'queue') !== false && strpos($key, 'queue_driver') === false){
+        $tryKey = substr($key, 10) . 'tries'; //get queue $key + tries to see if custom tries is set
+        $sleepKey = substr($key, 5) . 'sleep'; //get queue $key + sleep to see if custom sleep is set
+        $numProcKey = substr($key, 5) . 'numprocs'; //get queue $key + num process to see if custom number of processes is set
+        $startSecsKey = substr($key, 5) . 'startsecs'; //get queue $key + number of seconds the process should stay up
 
         $tries = isset($envVars[$tryKey]) ? $envVars[$tryKey] : 5;
         $sleep = isset($envVars[$sleepKey]) ? $envVars[$sleepKey] : 5;
         $numProcs = isset($envVars[$numProcKey]) ? $envVars[$numProcKey] : 1;
         $startSecs = isset($envVars[$startSecsKey]) ? $envVars[$startSecsKey] : 1;
-        $connection = isset($envVars['QUEUE_DRIVER']) ? $envVars['QUEUE_DRIVER'] : 'beanstalkd';
+        $connection = isset($envVars['queue_driver']) ? $envVars['queue_driver'] : 'beanstalkd';
         $programs .= generateProgram($connection, $val, $tries, $sleep, $numProcs, $startSecs);
     }
 }
